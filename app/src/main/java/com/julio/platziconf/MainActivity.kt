ac23eb11e38263c578ae86bd0c1bf0483b888483
Julio de la Lima <csar_13_lil@hotmail.com>
2020-03-31 14:43:45 -0600

    First data configuration

--- app/src/main/java/com/julio/platziconf/MainActivity.kt
+++ app/src/main/java/com/julio/platziconf/MainActivity.kt
@@ -2,11 +2,26 @@ package com.julio.platziconf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.firebase.firestore.FirebaseFirestore
+import com.julio.platziconf.model.Conference
+import com.julio.platziconf.model.Speaker
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBar(findViewById(R.id.tbMainToolbar))
+        configNav()
+
+    }
+    fun configNav(){
+        NavigationUI.setupWithNavController(bnMainMenu, Navigation.findNavController(this,R.id.fragMainContent))
     }
 }
